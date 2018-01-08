a3165a58926823de8f126b71cfd5735fdecf784e
Shuhei Miyasaka <shuhei.miyasaka@gmail.com>
2018-01-07 21:59:27 -0500

    Update db query code to use sql api

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,15 +1,27 @@
 # "Database code" for the DB Forum.
 
 import datetime
+import psycopg2
+import bleach
 
-POSTS = [("This is the first post.", datetime.datetime.now())]
+#POSTS = [("This is the first post.", datetime.datetime.now())]
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  conn = psycopg2.connect("dbname=forum")
+  cur = conn.cursor()
+  cur.execute("select content, time from posts order by time desc")
+  entries = cur.fetchall()
+  cur.close()
+  conn.close()
+  return entries
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
-
-
+  conn = psycopg2.connect("dbname=forum")
+  cur = conn.cursor()
+  cur.execute("insert into posts (content) VALUES (%s)", 
+    (bleach.clean(content),))
+  conn.commit()
+  cur.close()
+  conn.close()
