bc6d6da9dab38f6e8de9f8223aedd97deabc9a44
Shuhei Miyasaka <shuhei.miyasaka@gmail.com>
2018-01-11 10:34:14 -0500

    Set up initial db

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,22 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE IF EXISTS tournament;
 
+CREATE DATABASE tournament;
+
+\c tournament;
+
+CREATE TABLE players (
+    player_id serial PRIMARY KEY,
+    full_name text,
+    num_matches integer,
+    num_wins integer
+);
+
+CREATE TABLE matches (
+    match_id serial,
+    round integer,
+    winner_id integer REFERENCES players(player_id),
+    lower_id integer REFERENCES players(player_id)
+);
